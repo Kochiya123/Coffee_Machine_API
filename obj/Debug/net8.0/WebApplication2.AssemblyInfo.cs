694643c1062a64d02b320448c009b4c740396267
Kochiya123 <quanhvse184756@fpt.edu.vn>
2025-03-24 14:11:20 +0700

    Update Controller

--- obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebe7b72ad5243c82d97d944d61f417e4a6fe1d00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ea16811a178f518ab64806d8bbcb2135bf12d2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

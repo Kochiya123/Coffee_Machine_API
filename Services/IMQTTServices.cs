d3dae87ab6f3bcefb1d3cd3b8cd0b3b9b4f1eda4
Nguyễn Cơ Hiếu <hieuncse183228@fpt.edu.vn>
2025-03-28 00:43:28 +0700

    Mqtt changes

---
+++ Services/IMQTTServices.cs
@@ -0,0 +1,28 @@
+﻿ public class MQTTService : IMQTTService
+{
+    private readonly IMqttClient _mqttClient;
+
+    public MQTTService()
+    {
+        var factory = new MqttFactory();
+        _mqttClient = factory.CreateMqttClient();
+
+        var options = new MqttClientOptionsBuilder()
+            .WithClientId("BackendAPI")
+            .WithTcpServer("broker.hivemq.com", 1883)
+            .Build();
+
+        _mqttClient.ConnectAsync(options).Wait();
+    }
+
+    public async Task PublishOrder(OrderRequest order)
+    {
+        var message = new MqttApplicationMessageBuilder()
+            .WithTopic("order/new")
+            .WithPayload(JsonConvert.SerializeObject(order))
+            .WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce)
+            .Build();
+
+        await _mqttClient.PublishAsync(message);
+    }
+}
--- obj/Release/net8.0/WebApplication2.AssemblyInfo.cs
+++ obj/Release/net8.0/WebApplication2.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a72cffee0d02f5025b9ed45cc1b270819f0bf54")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
